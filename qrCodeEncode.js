d68966705fc41f6861577b373233030b4d340ece
Florian <florian@rnpvision.com>
2024-06-25 15:52:04 +0200

    minimum size changed to 90

--- qrCodeEncode.js
+++ qrCodeEncode.js
@@ -4,6 +4,7 @@ import clipboard from 'clipboardy';
 const prompt = promptSync();
 
 const url = prompt('Enter the url to encode for QR Code : ');
+const size = prompt('Enter the size of QR Code ( tip 85 is great for maps coords ) : ')
 
 const regex = new RegExp('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})');
 
@@ -16,7 +17,7 @@ new Promise((resolve, reject) => {
         reject(new Error('The URL isn\'t valid'));
     }
 }).then(() => {
-    const resultUrl = 'https://quickchart.io/qr?size=100&text=' + url.replaceAll('?', '%3F').replaceAll('&', '%26').replaceAll('=', '%3D').replaceAll('+', '%2B');
+    const resultUrl = `https://quickchart.io/qr?size=${size < 90 ? 90 : size}&text=` + url.replaceAll('?', '%3F').replaceAll('&', '%26').replaceAll('=', '%3D').replaceAll('+', '%2B');
 
     clipboard.writeSync(resultUrl);
     console.log('\n'  + '\x1b[38;5;42m' + resultUrl + '\x1b[0m' + ' copied in clipboard')
